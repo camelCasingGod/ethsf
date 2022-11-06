6c0e71a06ee9a48bbde1ee4320c252fa86366f91
Ameya Phadnis <aphadnis@berkeley.edu>
2022-11-05 20:36:32 -0700

    Added a few things

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Wallet } from 'ethers';
+import { Client } from '@xmtp/xmtp-js';
+import { useState } from 'react';
+// import { WorldIDWidget } from "@worldcoin/id";
+
+// var xmtpClient;
+
+// async function initializeXMTP() {
+//   let xmtp = await Client.create(wallet);
+//   console.log(xmtp);
+// }
 
 function App() {
+  const [message, setMessage] = useState(''); // checks textfield
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+  const handleClick = () => {
+    if (!(message === "")) {
+      setMessage('');
+      console.log(message);
+    } else {
+      console.log("Please... enter a message. Last warning.");
+    }
+  };
+
+  // initializeXMTP();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Messaging">
+          <input
+            type="text"
+            placeholder='message...'
+            id="message"
+            name="message"
+            onChange={handleChange}
+            value={message}
+          />
+          <br></br>
+          <button onClick={handleClick}>Send</button>
+          <br></br>
+          <p id="error"></p>
+          <br></br>
+        </div>
       </header>
     </div>
   );
